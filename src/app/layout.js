c135d4fa6b2cc5a2cfbeac2d5c21961f0dd981d1
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-03-06 11:51:35 +0000

    update metedata + add Poppins font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,26 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"]
 });
 
 export const metadata = {
   title: "Blake Harris - Junior Software Developer",
   description: "Junior Android developer working in GPS",
+  metadataBase: new URL("https://blakeharris.dev"),
+  openGraph: {
+    title: "Blake Harris - Junior Software Developer",
+    description: "Junior Android developer working in GPS",
+    url: "https://blakeharris.dev",
+    type: "website"
+  }
 };
 
 export default function RootLayout({ children }) {
@@ -26,7 +29,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={poppins.variable}>
         <Navbar />
         <Header />
         {children}
