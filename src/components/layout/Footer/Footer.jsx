91d575ca418e706c6e15e931273139ebafbdcf12
BHarris02 <dev.blake.harris@gmail.com>
2026-03-10 15:52:45 +0000

    Adds Footer

---
+++ src/components/layout/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import { socials } from "@/constants/socials";
+
+export default function Footer() {
+    return (
+        <footer id="contact" className="container py-5">
+            <hr className="border-secondary mb-5" />
+            <div>
+                <h2 className="text-white fw-bold mb-2">Get In Touch</h2>
+                <p className="text-secondary mb-4"> Feel free to reach out via LinkedIn or checkout my work on GitHub.</p>
+                <div className="d-flex gap-4 mb-5">
+                    {socials.map((social) => (
+                        <a 
+                        key={social.name}
+                        href={social.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-white fs-3"
+                        >
+                            <i className={social.icon}></i>
+                        </a>
+                    ))}
+                </div>
+            </div>
+            <p className="text-secondary small mb-0">
+                &copy; {new Date().getFullYear()} Blake Harris. All rights reserved.
+            </p>
+        </footer>
+    );
+}
---
+++ src/constants/socials.js
@@ -0,0 +1,12 @@
+export const socials = [
+    {
+        name: "GitHub",
+        icon: "devicon-github-original",
+        url: "https://github.com/BHarris02"
+    },
+        {
+        name: "LinkedIn",
+        icon: "devicon-linkedin-plain",
+        url: "https://linkedin.com/in/blake-harris-890039375/"
+    }
+]
